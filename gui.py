49d693c62bcf06fa48a2bf9df1f9408d0e0127d8
Kevin Henneberger <kevinhenneberger@Meghans-MacBook-Pro-2.local>
2017-04-18 10:47:24 -0400

    Added colors{} dictionary

--- gui.py
+++ gui.py
@@ -5,20 +5,6 @@ import button
 pygame.init()
 
 # global variables
-white = (255, 255, 255)
-black = (0, 0, 0)
-gray1 = (200, 200, 200)
-gray2 = (175, 175, 175)
-blue1 = (0, 0, 215)
-blue2 = (0, 0, 255)
-blue3 = (0, 0, 235)
-red1 = (215, 0, 0)
-red2 = (255, 0, 0)
-red3 = (235, 0, 0)
-green1 = (0, 135, 0)
-green2 = (0, 175, 0)
-green3 = (0, 155, 0)
-
 window_width = 400
 window_height = 500
 
@@ -27,7 +13,21 @@ game_board_y = 125
 game_board_width = 350
 game_board_height = 350
 
-# buttons
+colors = {}
+colors["white"] = (255, 255, 255)
+colors["black"] = (0, 0, 0)
+colors["gray1"] = (200, 200, 200)
+colors["gray2"] = (175, 175, 175)
+colors["blue1"] = (0, 0, 215)
+colors["blue2"] = (0, 0, 255)
+colors["blue3"] = (0, 0, 235)
+colors["red1"] = (215, 0, 0)
+colors["red2"] = (255, 0, 0)
+colors["red3"] = (235, 0, 0)
+colors["green1"] = (0, 135, 0)
+colors["green2"] = (0, 175, 0)
+colors["green3"] = (0, 155, 0)
+
 buttons = {}
 buttons["start"] = button.Button(100, "Start")
 buttons["help"] = button.Button(175, "Help")
@@ -44,7 +44,7 @@ buttons["play-again"] = button.Button(250, "Play Again")
 # initialize pygame
 window = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption('Minesweeper')
-window.fill(gray1)
+window.fill(colors["gray1"])
 
 def text(txt, x, y, font, size, color, align):
     """
@@ -79,24 +79,24 @@ def displayBoard(board, tile_width, tile_height, num_rows, num_cols):
             y = row * h + game_board_y
             
             if (board[row][col].isFlipped):
-                pygame.draw.rect(window, gray1, [x, y, w, h])
+                pygame.draw.rect(window, colors["gray1"], [x, y, w, h])
                 if (board[row][col].isMine()): 
                     text(board[row][col].value, x + (w / 2), y + h, "Impact", tile_width * 2, board[row][col].textColor(), "center")
                 else:
                     text(board[row][col].value, x + (w / 2), y + (h / 2), "Impact", tile_width, board[row][col].textColor(), "center")  
             else:
                 if (board[row][col].isFlagged):
-                    pygame.draw.rect(window, red1, [x, y, w, h])
-                    pygame.draw.rect(window, red2, [x + (w / 20), y + (h / 20), w - (w / 10), h - (h / 10)])
-                    pygame.draw.rect(window, red3, [x + (w / 4), y + (h / 4), w - (w / 2), h - (h / 2)])
+                    pygame.draw.rect(window, colors["red1"], [x, y, w, h])
+                    pygame.draw.rect(window, colors["red2"], [x + (w / 20), y + (h / 20), w - (w / 10), h - (h / 10)])
+                    pygame.draw.rect(window, colors["red3"], [x + (w / 4), y + (h / 4), w - (w / 2), h - (h / 2)])
                 elif (board[row][col].isMarked):
-                    pygame.draw.rect(window, green1, [x, y, w, h])
-                    pygame.draw.rect(window, green2, [x + (w / 20), y + (h / 20), w - (w / 10), h - (h / 10)])
-                    pygame.draw.rect(window, green3, [x + (w / 4), y + (h / 4), w - (w / 2), h - (h / 2)])
+                    pygame.draw.rect(window, colors["green1"], [x, y, w, h])
+                    pygame.draw.rect(window, colors["green2"], [x + (w / 20), y + (h / 20), w - (w / 10), h - (h / 10)])
+                    pygame.draw.rect(window, colors["green3"], [x + (w / 4), y + (h / 4), w - (w / 2), h - (h / 2)])
                 else:
-                    pygame.draw.rect(window, blue1, [x, y, w, h])
-                    pygame.draw.rect(window, blue2, [x + (w / 20), y + (h / 20), w - (w / 10), h - (h / 10)])
-                    pygame.draw.rect(window, blue3, [x + (w / 4), y + (h / 4), w - (w / 2), h - (h / 2)])
+                    pygame.draw.rect(window, colors["blue1"], [x, y, w, h])
+                    pygame.draw.rect(window, colors["blue2"], [x + (w / 20), y + (h / 20), w - (w / 10), h - (h / 10)])
+                    pygame.draw.rect(window, colors["blue3"], [x + (w / 4), y + (h / 4), w - (w / 2), h - (h / 2)])
          
 def gameLoop():
     """
@@ -141,14 +141,14 @@ def gameLoop():
             pygame.display.update()
 
             # GUI display
-            window.fill(gray1)
-            text("Minesweeper", window_width / 2, 50, "Impact", 48, white, "center")  
+            window.fill(colors["gray1"])
+            text("Minesweeper", window_width / 2, 50, "Impact", 48, colors["white"], "center")  
             displayButton(buttons["start"])
             displayButton(buttons["help"])
             displayButton(buttons["settings"])
             displayButton(buttons["high_scores"])
-            text("Created by Kevin Henneberger, Adam Wiener,", window_width / 2, 425, "Impact", 16, white, "center")  
-            text("Matt Aber, and Baptiste Saliba", window_width / 2, 450, "Impact", 16, white, "center")
+            text("Created by Kevin Henneberger, Adam Wiener,", window_width / 2, 425, "Impact", 16, colors["white"], "center")  
+            text("Matt Aber, and Baptiste Saliba", window_width / 2, 450, "Impact", 16, colors["white"], "center")
 
             # event handling
             for event in pygame.event.get():
@@ -180,8 +180,8 @@ def gameLoop():
             pygame.display.update()
 
             # GUI display
-            window.fill(gray1)
-            text("Help", window_width / 2, 50, "Impact", 48, white, "center")
+            window.fill(colors["gray1"])
+            text("Help", window_width / 2, 50, "Impact", 48, colors["white"], "center")
 
             instructions = [
                 "Insert step 1 here...", 
@@ -192,7 +192,7 @@ def gameLoop():
             ]
 
             for i in range(len(instructions)):
-                text(str(i + 1) + ") " + instructions[i], 35, i * 35 + 100, "Impact", 18, white, "left") 
+                text(str(i + 1) + ") " + instructions[i], 35, i * 35 + 100, "Impact", 18, colors["white"], "left") 
 
             displayButton(buttons["help_back"])
 
@@ -221,8 +221,8 @@ def gameLoop():
             pygame.display.update()
 
             # GUI display
-            window.fill(gray1)
-            text("Settings", window_width / 2, 50, "Impact", 48, white, "center")
+            window.fill(colors["gray1"])
+            text("Settings", window_width / 2, 50, "Impact", 48, colors["white"], "center")
             displayButton(buttons["easy"])
             displayButton(buttons["medium"])
             displayButton(buttons["hard"])  
@@ -272,8 +272,8 @@ def gameLoop():
             pygame.display.update()
 
             # GUI display
-            window.fill(gray1)
-            text("High Scores", window_width / 2, 50, "Impact", 48, white, "center")  
+            window.fill(colors["gray1"])
+            text("High Scores", window_width / 2, 50, "Impact", 48, colors["white"], "center")  
             displayButton(buttons["high_scores_back"])
 
             high_scores_list = [
@@ -285,7 +285,7 @@ def gameLoop():
             ]
 
             for i in range(len(high_scores_list)):
-                text(str(i + 1) + ") " + high_scores_list[i], 35, i * 35 + 100, "Impact", 18, white, "left") 
+                text(str(i + 1) + ") " + high_scores_list[i], 35, i * 35 + 100, "Impact", 18, colors["white"], "left") 
 
             displayButton(buttons["help_back"])
 
@@ -312,7 +312,7 @@ def gameLoop():
             # update pygame frame
             pygame.display.update()
 
-            window.fill(gray2)
+            window.fill(colors["gray2"])
 
             # GUI display of board
             displayBoard(game_board.board, tile_width, tile_height, num_rows, num_cols)
@@ -383,7 +383,7 @@ def gameLoop():
             # GUI display
             game_board.revealMines()
             displayBoard(game_board.board, tile_width, tile_height, num_rows, num_cols)
-            text("GAME OVER!", window_width / 2, 175, "Impact", 48, white, "center")  
+            text("GAME OVER!", window_width / 2, 175, "Impact", 48, colors["white"], "center")  
             displayButton(buttons["play-again"])
 
             # event handling
@@ -413,7 +413,7 @@ def gameLoop():
             # GUI display
             displayBoard(game_board.board, tile_width, tile_height, num_rows, num_cols)
             font = pygame.font.SysFont("Impact", 48)
-            text("YOU WIN!", window_width / 2, 175, "Impact", 48, white, "center")  
+            text("YOU WIN!", window_width / 2, 175, "Impact", 48, colors["white"], "center")  
             displayButton(buttons["play-again"])
 
             # event handling
